e7ddb13be49acf73a814489ea7b327d6e412bc78
Purna-ASU <ppeddire@asu.edu>
2021-11-29 21:55:46 -0700

    US76-Task99 and US76-Task100 Added marker and address to map

--- client/src/pages/map.js
+++ client/src/pages/map.js
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+import * as parkData from "./venues.json";
+import mapStyles from "./mapStyles";
 
-const Maps = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Center',
-		alignItems: 'Top',
-		height: '100vh'
-	}}
-	>
-	<h1>Find All Tournament Venues!!</h1>
-	</div>
-);
-};
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
 
-export default Maps;
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <GoogleMap
+      defaultZoom={15}
+      defaultCenter={{ lat: 33.424018, lng: -111.934284 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {parkData.features.map((park) => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0],
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0],
+          }}
+        >
+          <div>
+            <h3>{selectedPark.properties.NAME}</h3>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDsRQnoE8lCjH91KBs2WNqgewdImJ7s8Ww`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
---
+++ client/src/pages/mapStyles.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
